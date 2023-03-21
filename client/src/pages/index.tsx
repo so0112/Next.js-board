489eb0fea8f30e9f7e4a852971feca3601c33f1b
so0112 <tkddh0112@gmail.com>
2023-03-21 23:05:23 +0900

    Init: Next.js 초기설정
    - TypeScript
    - Styled-Components
    - babelrc

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </>
+  );
+}
